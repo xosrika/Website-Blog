03c887e2c850e9e10eb253a1c191748b2d3456f1
xosrika <gkhos15@freeuni.edu.ge>
2018-04-07 21:33:23 +0400

    Added login and sign up pages.

---
+++ signup.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+  <head>
+	<style>
+		.group:after {
+		  content: "";
+		  display: table;
+		  clear: both;
+		}
+
+		* {
+				box-sizing: border-box;
+		}
+		body {margin:0;}
+
+		#navbar ul {
+			
+				position: fixed;
+				top: 0;
+				width: 100%;
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+				overflow: hidden;
+				background-color: steelblue;
+		}
+
+		#navbar .leftBar {
+				float: left;
+				
+		}
+
+		#navbar .rightBar{
+				float: right;
+				
+		}
+
+		#navbar li a {
+				display: block;
+				color: white;
+				text-align: center;
+				padding: 14px 16px;
+				text-decoration: none;
+		}
+
+		#navbar li a:hover:not(.active) {
+				background-color: cornflowerblue;
+		}
+
+		#navbar .active {
+				background-color: darkblue;
+		}
+
+		
+		.column {
+				float: left;
+				padding: 10px;
+		}
+		.column.side {
+				
+				width: 10%;
+		}
+
+		.column.middle {
+				width: 80%;
+		}
+	
+		#content{
+			padding:20px;
+			margin-top:30px;
+			
+		}	
+
+		#content:after {
+				content: "";
+				display: table;
+				clear: both;
+		}
+		
+		#logo{		
+			float: left;			
+		}
+		
+
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+
+        input[type=password], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }    
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+
+        #form {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+            margin: auto;
+			width: 60%;
+        }
+			
+	</style>
+    <meta charset="utf-8">
+		<meta name="author" content="Giorgi Khosroshvili">
+		<meta name="description" content="This is the home page of blog site. 
+		Here you can see all kind of information.">
+    <title>Blog</title>
+  </head>
+  <body>
+		
+    <div id="navbar" class="group">
+	
+			
+		<ul class="group">
+			<li> <img id="logo" src="blogicon.png" id="log" height="48" width="48"></li>
+			<li><a class="leftBar" href="index.php">Home Page</a></li>
+			<li><a class="leftBar" href="newposts.php">New Posts</a></li>
+	
+			<li><a class="rightBar" href="login.php">Log In</a></li>
+			<li><a class="rightBar active" href="signup.php">Sign Up</a></li>
+		</ul>
+		
+    </div> <!-- #navbar -->
+    
+
+	<div id="content"> 
+		<div class="column side">
+			<h2>Side</h2>
+			
+			<p></p>
+		</div>  <!-- left column -->
+
+		<div class="column middle">
+            <form id="form" style="margin-top: 50px;" action=<?php echo $_SERVER['PHP_SELF']?> >
+                <label for="nickname">Nickname</label>
+                <input type="text" id="nickname" name="firstname" placeholder="Your nickname..">
+				
+                <label for="email">Email</label>
+                <input type="text" id="email" name="mail" placeholder="Your email..">
+
+                <label for="password">Password</label>
+                <input type="password" id="lnapasswordme" name="lastname" placeholder="">
+
+                <input type="submit" value="Sign Up">
+            </form>
+		</div> <!-- column middle -->
+		 
+		<div class="column side">
+			<h2>Side</h2>
+			<p></p>
+		</div> <!-- right column -->
+	</div> <!-- #content -->
+	</body>
+		
+
+</html>
